94aeb83a944ffba38a09a477b2585e1fed868070
Jalu Satriani Wijaya <jalusatrianiwijaya1@gmail.com>
2024-10-30 21:41:41 +0800

    fix missing import

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 
 import * as user from "./schema/user.schema";
-import * as product from "./schema/product.schema";
 
 const createServer = () => {
   const server = new ApolloServer({
-    typeDefs: [user.types, product.types],
-    resolvers: [user.resolvers, product.resolvers],
+    typeDefs: [user.types],
+    resolvers: [user.resolvers],
   });
   return server;
 };
